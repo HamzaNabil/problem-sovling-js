3fdeb832e5702736b089bbeeca80e980653d238b
HamzaNabil39 <hamza.nabil@izam.co>
2022-09-23 23:21:18 +0200

    add swapping js

---
+++ swaping.js
@@ -0,0 +1,20 @@
+// swap two numbers
+
+let cupOfOrangeJuice = 'orange juice';
+let cupOfStrawberryJuice = 'strawberry juice';
+let emptyCup = '';
+
+// emptyCup = cupOfOrangeJuice;
+// cupOfOrangeJuice = cupOfStrawberryJuice;
+// cupOfStrawberryJuice = emptyCup;
+
+// Destruct
+[cupOfOrangeJuice, cupOfStrawberryJuice] = [cupOfStrawberryJuice, cupOfOrangeJuice];
+
+console.log(cupOfOrangeJuice);
+console.log(cupOfStrawberryJuice);
+
+
+
+
+
